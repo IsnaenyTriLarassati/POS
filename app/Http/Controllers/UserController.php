6b09c1a2f8be2dad51a5c77c534afc6772f808ac
isnaenytrilarassati <isnaenytrilarassati>
2025-04-08 14:19:59 +0700

    Praktikum 2.1 – Retrieving Single Models -J4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
 
     public function index() 
     {
-        $data = [
+        //$data = [
             // 'level_id' => 2,
             // 'username' => 'manager_dua',
             // 'nama' => 'Manager 2',
@@ -21,14 +21,35 @@ class UserController extends Controller
 
             //Modifikasi J-04 $fillable
 
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345') 
-          ];
-          UserModel::create($data);
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345') 
+        //   ];
+        //   UserModel::create($data);
   
-          $user = UserModel::all();
-          return view('user', ['data' => $user]);
+        //   $user = UserModel::all();
+        //   return view('user', ['data' => $user]);
+
+
+        
+        //--
+        //Praktikum 2.1 – Retrieving Single Models -J4
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+
+        //Modifikasi
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
+        //Modifikasi
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        //Modifikasi
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
+        return view('user', ['data' => $user]);
     }
 }
