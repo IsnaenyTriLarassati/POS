55debfaf73b81cf890563b2adffb2b6431c969f3
isnaenytrilarassati <isnaenytrilarassati>
2025-04-08 16:14:25 +0700

    Praktikum 2.5 – Attribute Changes - J4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,48 +8,47 @@ class UserController extends Controller
 {
     public function index() 
     {
-        // $user = UserModel::firstOrCreate(
-        //     [
-        //         'username' => 'manager',
-        //         'nama' => 'Manager'
-        //     ]
-        // );
+        // $user = UserModel::create([
+        //     'username' => 'manager55',
+        //     'nama' => 'Manager55',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
 
-        //return view('user', ['data' => $user]);
+        // $user->username = 'manager56';
 
-        //Modifikasi no.4
-        // $user = UserModel::firstOrCreate(
-        //     [
-        //         'username' => 'manager22',
-        //         'nama' => 'Manager Dua Dua',
-        //         'password' => Hash::make('12345'),
-        //         'level_id' => 2
-        //     ]
-        // );
+        // $user->isDirty(); // true
+        // $user->isDirty('username'); // true
+        // $user->isDirty('nama'); // false
+        // $user->isDirty(['nama', 'username']); // true
 
-        //return view('user', ['data' => $user]);
+        // $user->isClean(); // false
+        // $user->isClean('username'); // false
+        // $user->isClean('nama'); // true
+        // $user->isClean(['nama', 'username']); // false
 
-        // Modifikasi no.6
-        // $user = UserModel::firstOrNew(
-        //     [
-        //         'username' => 'manager',
-        //         'nama' => 'Manager',
-        //     ]
-        // );
+        // $user->save();
 
-        //return view('user', ['data' => $user]);
+        // $user->isDirty(); // false
+        // $user->isClean(); // true
+        // dd($user->isDirty());
 
-        //Modifikasi no.8
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ]
-        );
+        //Modifikasi no.3
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager12';
+
         $user->save();
 
-        return view('user', ['data' => $user]);
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username'])); // true
     }
 }
