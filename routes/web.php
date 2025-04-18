67ab4b343ac2f98a70cb7d1a68669b8dde1f0684
isnaenytrilarassati <isnaenytrilarassati>
2025-04-18 21:12:04 +0700

    Praktikum 3. - Modal Ajax Hapus Data (Data User) -J6

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,15 @@ Route::group(['prefix' => 'user'], function() {
     Route::post('/list', [UserController::class, 'list']);          //Menampilkan data user dalam bentuk json untuk database
     Route::get('/create', [UserController::class, 'create']);       //Menampilkan halaman form tambah user
     Route::post('/', [UserController::class, 'store']);             //Menyimpan data user baru
-    Route::get('/create_ajax', [UserController:: class, 'create_ajax']);    //Menampilkan halaman form tambahan user ajax
-    Route::post('/ajax', [UserController::class, 'store_ajax']);    //Menyimpan data user baru ajax
+    Route::get('/create_ajax', [UserController:: class, 'create_ajax']);        //Menampilkan halaman form tambahan user ajax
+    Route::post('/ajax', [UserController::class, 'store_ajax']);                //Menyimpan data user baru ajax
     Route::get('/{id}', [UserController::class, 'show']);           //Menampilkan detail user
     Route::get('/{id}/edit', [UserController::class, 'edit']);      //Menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']);         //Manyimpan perubahan data user
-    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);    //Menampilkan halaman form edit user ajax
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);        //Menampilkan halaman form edit user ajax
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);    //Menyipan perubahan data user ajax
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);   //Untuk tampilkan from confirm delete user ajax
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); //User hapus data user Ajax
     Route::delete('/{id}', [UserController::class, 'destroy']);     //Menghapus data user
 });
 
