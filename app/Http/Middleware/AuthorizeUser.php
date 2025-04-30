f5b9cc24f26b1d9589c7754d58cfe90cc32ee427
isnaenytrilarassati <isnaenytrilarassati>
2025-04-30 22:53:31 +0700

    Jobsheet 7 - Praktikum 3

--- app/Http/Middleware/AuthorizeUser.php
+++ app/Http/Middleware/AuthorizeUser.php
@@ -13,14 +13,12 @@ class AuthorizeUser
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, string $role = ''): Response
+    public function handle(Request $request, Closure $next, ... $roles): Response
     {
-        $user = $request->user(); // ambil data user yg login
-        // fungsi user() diambil dari UserMode.php
-        if ($user && $user->hasRole($role)) { // cek apakah user punya role yg diinginkan
-            return $next($request);
+        $user_role = $request->user()->getRole();   // ambil data level_kode dari user yang login
+        if(in_array($user_role, $roles)) {
+            return $next($request);  // cek apakah user punya role yang diinginkan
         }
-
         // jika tidak punya role, maka tampilkan error 403
         abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini!');
     }
