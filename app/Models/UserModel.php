3711e522d7e52f407c59bac45aeb3029fdc18198
isnaenytrilarassati <isnaenytrilarassati>
2025-04-30 15:53:43 +0700

    Jobsheet 7 - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,20 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+use Illuminate\Database\Eloquent\Model;
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
-    
-    protected $table = 'm_user'; 
-    protected $primaryKey = 'user_id';
 
+    protected $table = 'm_user';       // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    public function level()
-    {
-    return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
+    protected $hidden = ['password']; // jangan di tampilkan saat select
+
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
+
 }
