2a0e4e9937f0af7a5a933e562bbaad70ec501aa2
isnaenytrilarassati <isnaenytrilarassati>
2025-04-30 23:04:27 +0700

    Jobsheet 7 - Tugas Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -48,4 +48,28 @@ class AuthController extends Controller
         return redirect('login');
     }
 
+    public function register()
+    {
+        $levels = LevelModel::all();
+        return view('auth.register', ['level' => $levels]);
+    }
+
+    public function postRegister(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama' => 'required|string|max:100', 
+            'password' => 'required|min:5', 
+            'level_id' => 'required|integer', 
+        ]);
+    
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => bcrypt($request->password), 
+            'level_id' => $request->level_id
+        ]);
+    
+        return redirect('login')->with('success', 'Registrasti berhasil ditambahkan');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ Route::pattern('id', '[0-9]+'); // artinya ketika ada parameter {id}, maka harus
 
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postlogin']);
-// Route::get('logout', [AuthController::class, 'logout'])->middleware('logout');
+Route::get('register', [AuthController::class, 'register']);
+Route::post('register', [AuthController::class, 'postRegister']);
+Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
 Route::get('logout', [AuthController::class, 'logout']);
 
 
